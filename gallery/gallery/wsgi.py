802d6863c5bcc5a8c0f594996ccc1c2c543f79d6
priscillapepe <priscillaungai99@gmail.com>
2020-10-12 18:19:24 +0300

    changed names of folder to gallery

--- gallery/gallery/wsgi.py
+++ gallery/gallery/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mypics project.
+WSGI config for gallery project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery.settings')
 
 application = get_wsgi_application()
